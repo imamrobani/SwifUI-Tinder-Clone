80724486dacee8ec2b21fb0b7b0ce7fea3aebf34
Imam Robani <imamrob@gmail.com>
2024-03-17 09:19:14 +0700

    :wrench: setup project

---
+++ TinderClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabView.swift
+//  TinderClone
+//
+//  Created by Imam Robani on 17/03/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Swiping View")
+                .tabItem { Image(systemName: "flame") }
+                .tag(0)
+            
+            Text("Search View")
+                .tabItem { Image(systemName: "magnifyingglass") }
+                .tag(1)
+            
+            Text("Inbox View")
+                .tabItem { Image(systemName: "bubble") }
+                .tag(2)
+            
+            Text("Profile View")
+                .tabItem { Image(systemName: "person") }
+                .tag(3)
+        }
+        .tint(.primary)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
