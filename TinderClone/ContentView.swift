c31ea6572639c5a43479c5ddbe7625479d865df8
Imam Robani <imamrob@gmail.com>
2024-03-17 09:06:28 +0700

    Initial Commit

---
+++ TinderClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TinderClone
+//
+//  Created by Imam Robani on 17/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Imam Robani on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
